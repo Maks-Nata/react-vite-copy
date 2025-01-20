ffd3a3588b830b82833b982d1a6adead750baeac
Maks-Nata <mixcim20121984@gmail.com>
2025-01-20 12:57:37 +0200

    Why don't work env?

--- src/services/general.api.services.ts
+++ src/services/general.api.services.ts
@@ -1,5 +1,5 @@
-const  baceURL='https://dummyjson.com'
-
+const  baceURL=import.meta.env.VITE_BASE_URL
+console.log(baceURL)
 export const getUsers = async <T>(): Promise<T> => {
   return    await fetch(`${baceURL}/users`).then(res => res.json())
 
